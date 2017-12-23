63193ad9ec8e413625ad40ca361f22d53b85ccd1
Thomas Nelson <tanelso2@gmail.com>
2017-12-22 20:22:36 -0800

    Day 17 done?
    We'll see how long it takes to calculate the part two answer

---
+++ day17/src/day17/core.clj
@@ -0,0 +1,39 @@
+(ns day17.core
+  (:gen-class))
+
+(defn insert-next
+  [curr-pos curr-list number-of-steps next-val]
+  (let [length (count curr-list)
+        split-pos (inc (mod (+ curr-pos number-of-steps) length))
+        [head tail] (split-at split-pos curr-list)]
+    [(concat head (list next-val) tail)
+     split-pos]))
+
+(defn puzzle1
+  [number-of-steps]
+  (letfn [(reduction-fn
+            [[curr-list curr-pos] next-val]
+            (insert-next curr-pos curr-list number-of-steps next-val))]
+    (->> (range 1 2018)
+      (reduce reduction-fn ['(0) 0])
+      (first)
+      (drop-while #(not= 2017 %))
+      (second))))
+
+(defn puzzle2
+  [number-of-steps]
+  (letfn [(reduction-fn
+            [[curr-list curr-pos] next-val]
+            (insert-next curr-pos curr-list number-of-steps next-val))]
+    (->> (range 1 (inc 50000000))
+         (reduce reduction-fn ['(0) 0])
+         (first)
+         (drop-while #(not= 0 %))
+         (second))))
+
+(def puzzle-input 303)
+
+(defn -main
+  [& args]
+  (println (time (puzzle1 puzzle-input)))
+  (println (time (puzzle2 puzzle-input))))
